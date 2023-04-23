4c2cfef61653bf150a5e4da4ebffe865be34a146
AndreFoulem <timmycasual@icloud.com>
2023-04-23 16:50:55 -0700

    Initial Commit

---
+++ Design_Patterns/Design_PatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_PatternsApp.swift
+//  Design_Patterns
+//
+//  Created by AndreMacBook on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct Design_PatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
