4439c9cf90fe2b9c8e634e9d502b7564c510ef0b
AndreFoulem <timmycasual@icloud.com>
2023-04-23 16:58:13 -0700

    Create the singleton 'shared' prop

---
+++ Design_Patterns/Singleton/UserSettings.swift
@@ -0,0 +1,23 @@
+//
+//  UserSettings.swift
+//  Design_Patterns
+//
+//  Created by AndreMacBook on 2023-04-23.
+//
+
+import Foundation
+
+final public class UserSettings {
+  
+  private var settings: [String: Any] = [
+    "Theme": "Vivid", "Brightness": 50
+  ]
+  
+  //-> Hide the initializer
+  private init() {}
+  
+  public static let shared = UserSettings()
+  
+  
+  
+}
